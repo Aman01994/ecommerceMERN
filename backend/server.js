5cfe7965c9547fa145fc1dc8e48da971bf33a7a2
Aman01994 <aman.kohli.2009@gmail.com>
2025-07-16 15:49:36 +0530

    cors problem solved

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,19 @@ const OrdersRoute = require('./routes/orderRoute');
 
 
 // Allow all origins
-app.use(cors());
+const allowedOrigins = ['https://www.foreverclothes.xyz', 'https://ecommerceadminpanel-jet.vercel.app/'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 
 Port = process.env.PORT || 4000
 
